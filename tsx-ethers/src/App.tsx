55c67e1783e3a002b23f34a8421c44181205989a
Kyle Lynds <kplynds3@gmail.com>
2022-04-06 19:24:47 -0400

    first commit

---
+++ tsx-ethers/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+// import { ethers } from "ethers";
+
+function App() {
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
